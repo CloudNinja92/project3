5811b40441af675f30a5eec5f6048c9b7de7f02c
CloudNinja92 <144071013+CloudNinja92@users.noreply.github.com>
2023-12-09 20:18:59 -0800

    Update 00-Auth.tf

--- 00-Auth.tf
+++ 00-Auth.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    aws= {
-        source = "hashicorp/aws"
-        version = "~> 3.0"
-    }
-  }
+provider "aws" {
+  region = "ap-northeast-3"
 }
 
-provider "aws" {
-   region = "eu-west-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 

f8c9b4f35b3fe85755039c97bb9a6a1c2d5f217c
CloudNinja92 <144071013+CloudNinja92@users.noreply.github.com>
2023-12-09 20:17:00 -0800

    Update 03-policies.tf

--- 03-policies.tf
+++ 03-policies.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "group_policies" {
 resource "aws_iam_group_policy" "group_specific_policy" {
   for_each = var.group_policies
   name     = "${each.key}-policy"
-  group    = aws_iam_group.group[each.key].name
+  group    = aws_iam_group.Akatsuki[each.key].name
   policy   = data.aws_iam_policy_document.group_policies[each.key].json
 }
 
@@ -32,6 +32,6 @@ data "aws_iam_policy_document" "mfa_policy" {
 resource "aws_iam_group_policy" "mfa_policy" {
   for_each = { for k in var.mfa_enabled_groups : k => var.iam_groups[k] if contains(keys(var.iam_groups), k) }
   name     = "${each.key}-mfa-policy"
-  group    = aws_iam_group.group[each.key].name
+  group    = aws_iam_group.Akatsuki[each.key].name
   policy   = data.aws_iam_policy_document.mfa_policy.json
 }
